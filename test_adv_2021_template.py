46743e0ddaafdb7383289c66fb60c7d5eae4d921
piotr.idzik <vil02@o2.pl>
2021-11-30 19:54:42 +0100

    added template

---
+++ test_adv_2021_template.py
@@ -0,0 +1,30 @@
+"""
+test for adv_2015_template
+"""
+
+import unittest
+# import adv_2015_template as sol
+# import general_utils as gu
+
+
+class TestSolutionA(unittest.TestCase):
+    """
+    unit tests for part a
+    """
+    # def test_basic(self):
+    #     """test agains the example data"""
+    #     pass
+    #
+    # def test_data_p(self):
+    #     """test agains full data"""
+    #     pass
+
+
+class TestSolutionB(unittest.TestCase):
+    """
+    unit tests for part b
+    """
+
+
+if __name__ == '__main__':
+    unittest.main()
