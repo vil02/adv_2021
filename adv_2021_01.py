74b23650b3d7897db88277ae8b2ecb5e90c4fa67
piotr.idzik <vil02@o2.pl>
2021-12-01 08:13:52 +0100

    added variable window_size in count_b

--- adv_2021_01.py
+++ adv_2021_01.py
@@ -19,9 +19,12 @@ def solve_a(in_str):
 
 def count_b(in_list):
     """counts the value from part b"""
-    sum_list = [sum(in_list[0:3])]
-    for cur_pos in range(3, len(in_list)):
-        sum_list.append(sum_list[-1]-in_list[cur_pos-3]+in_list[cur_pos])
+    window_size = 3
+    assert len(in_list) >= window_size
+    sum_list = [sum(in_list[0:window_size])]
+    for cur_pos in range(window_size, len(in_list)):
+        sum_list.append(
+            sum_list[-1]-in_list[cur_pos-window_size]+in_list[cur_pos])
     return count_a(sum_list)
 
 
