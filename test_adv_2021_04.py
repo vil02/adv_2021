f9cf81b8a552d1841dc1b3be8a4d1fa3fb44b5fd
piotr.idzik <vil02@o2.pl>
2021-12-04 06:50:53 +0100

    data_adv_2021_04_small.txt added

--- test_adv_2021_04.py
+++ test_adv_2021_04.py
@@ -6,6 +6,7 @@ import unittest
 import adv_2021_04 as sol
 import general_utils as gu
 
+_DATA_SMALL = gu.read_to_string('data_adv_2021_04_small.txt')
 _DATA_P = gu.read_to_string('data_adv_2021_04_p.txt')
 
 
@@ -17,6 +18,11 @@ class TestSolutionA(unittest.TestCase):
     #     """test agains the example data"""
     #     pass
     #
+    def test_data_small(self):
+        """test agains the example data"""
+        self.assertEqual(sol.solve_a(_DATA_SMALL), 4512)
+
+
     def test_data_p(self):
         """test agains full data"""
         self.assertEqual(sol.solve_a(_DATA_P), 54275)
@@ -26,6 +32,10 @@ class TestSolutionB(unittest.TestCase):
     """
     unit tests for part b
     """
+    def test_data_small(self):
+        """test agains the example data"""
+        self.assertEqual(sol.solve_b(_DATA_SMALL), 1924)
+
     def test_data_p(self):
         """test agains full data"""
         self.assertEqual(sol.solve_b(_DATA_P), 13158)
