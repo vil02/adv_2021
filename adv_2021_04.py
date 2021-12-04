43a16a3129470e5a0f6017cbee38a76db49106c7
piotr.idzik <vil02@o2.pl>
2021-12-04 07:03:36 +0100

    general cleaning

--- adv_2021_04.py
+++ adv_2021_04.py
@@ -13,9 +13,8 @@ def _parse_input(in_str):
         res = []
         for cur_line in in_lines:
             cur_line_res = []
-            for cur_num in cur_line.split(' '):
-                if cur_num != ' ' and cur_num != '':
-                    cur_line_res.append((int(cur_num), False))
+            for cur_num_str in cur_line.split():
+                cur_line_res.append((int(cur_num_str), False))
             assert(len(cur_line_res)) == _board_size()
             res.append(cur_line_res)
         return res
@@ -27,11 +26,11 @@ def _parse_input(in_str):
     res = []
     for start_num in range(0, len(lines), _board_size()):
         res.append(proc_square(lines[start_num:start_num+_board_size()]))
-    # print(res)
     return numbers, res
 
 
 def mark(in_square, in_num):
+    """returns in_square with marked in_num"""
     res_lines = []
     for cur_line in in_square:
         tmp_line = []
@@ -44,7 +43,10 @@ def mark(in_square, in_num):
     return res_lines
 
 
-def check_if_wins(in_square):
+def is_winning(in_square):
+    """
+    checks if in_square wins
+    """
     def is_row_marked(in_row_num):
         return all(_[1] for _ in in_square[in_row_num])
 
@@ -56,15 +58,24 @@ def check_if_wins(in_square):
 
 
 def find_winning(in_data):
+    """finds winning square"""
     res = None
     for _ in in_data:
-        if check_if_wins(_):
+        if is_winning(_):
             res = _
             break
     return res
 
-def count_unmarked_sum(in_data):
-    return sum(sum(_[0] for _ in cur_row if not _[1]) for cur_row in in_data)
+
+def count_score(in_data, in_num):
+    """returns the score of the winning square"""
+    def count_unmarked_sum(in_data):
+        """retuns the sum of unmarked fields"""
+        return sum(
+            sum(_[0] for _ in cur_row if not _[1]) for cur_row in in_data)
+    assert is_winning(in_data)
+    return count_unmarked_sum(in_data)*in_num
+
 
 def solve_a(in_str):
     """solution function for part a"""
@@ -73,7 +84,7 @@ def solve_a(in_str):
         data = [mark(_, cur_num) for _ in data]
         winner = find_winning(data)
         if winner is not None:
-            return count_unmarked_sum(winner)*cur_num
+            return count_score(winner, cur_num)
     return -2
 
 
@@ -85,6 +96,6 @@ def solve_b(in_str):
         data = [mark(_, cur_num) for _ in data]
         winner = find_winning(data)
         if winner is not None:
-            last_winner_score = count_unmarked_sum(winner)*cur_num
-        data = [_ for _ in data if not check_if_wins(_)]
+            last_winner_score = count_score(winner, cur_num)
+        data = [_ for _ in data if not is_winning(_)]
     return last_winner_score
