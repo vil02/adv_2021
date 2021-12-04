007cd235ccd2bd11d4104f3727fcb80f4ecea47c
piotr.idzik <vil02@o2.pl>
2021-12-04 06:47:38 +0100

    solution of adv_2021_04 added

---
+++ adv_2021_04.py
@@ -0,0 +1,86 @@
+"""
+solution of adv_2021_04
+"""
+import itertools
+
+
+def _parse_input(in_str):
+    def proc_square(in_lines):
+        assert len(in_lines) == 5
+        res = []
+        for cur_line in in_lines:
+            cur_line_res = []
+            for cur_num in cur_line.split(' '):
+                if cur_num != ' ' and cur_num != '':
+                    cur_line_res.append((int(cur_num), False))
+            assert(len(cur_line_res)) == 5
+            res.append(cur_line_res)
+        return res
+    lines = in_str.splitlines()
+    lines = [_ for _ in lines if len(_) > 1]
+    numbers = [int(_) for _ in lines[0].split(',')]
+    lines = lines[1:]
+    assert len(lines) % 5 == 0
+    res = []
+    for start_num in range(0, len(lines), 5):
+        res.append(proc_square(lines[start_num:start_num+5]))
+    # print(res)
+    return numbers, res
+
+
+def mark(in_square, in_num):
+    res_lines = []
+    for cur_line in in_square:
+        tmp_line = []
+        for _ in cur_line:
+            if _[0] == in_num:
+                tmp_line.append((_[0], True))
+            else:
+                tmp_line.append(_)
+        res_lines.append(tmp_line)
+    return res_lines
+
+
+def check_if_wins(in_square):
+    def is_row_marked(in_row_num):
+        return all(_[1] for _ in in_square[in_row_num])
+
+    def is_col_marked(in_col_num):
+        cur_col = [in_square[_][in_col_num] for _ in range(5)]
+        return all(_[1] for _ in cur_col)
+    return any(is_row_marked(_) for _ in range(5)) or \
+        any(is_col_marked(_) for _ in range(5))
+
+def find_winning(in_data):
+    res = None
+    for _ in in_data:
+        if check_if_wins(_):
+            res = _
+            break
+    return res
+
+def count_unmarked_sum(in_data):
+    return sum(sum(_[0] for _ in cur_row if not _[1]) for cur_row in in_data)
+
+def solve_a(in_str):
+    """solution function for part a"""
+    numbers, data = _parse_input(in_str)
+    for cur_num in numbers:
+        data = [mark(_, cur_num) for _ in data]
+        winner = find_winning(data)
+        if winner is not None:
+            return count_unmarked_sum(winner)*cur_num
+    return -2
+
+
+def solve_b(in_str):
+    """solution function for part b"""
+    numbers, data = _parse_input(in_str)
+    last_winner_score = None
+    for cur_num in numbers:
+        data = [mark(_, cur_num) for _ in data]
+        winner = find_winning(data)
+        if winner is not None:
+            last_winner_score = count_unmarked_sum(winner)*cur_num
+        data = [_ for _ in data if not check_if_wins(_)]
+    return last_winner_score
---
+++ test_adv_2021_04.py
@@ -0,0 +1,35 @@
+"""
+test for adv_2021_template
+"""
+
+import unittest
+import adv_2021_04 as sol
+import general_utils as gu
+
+_DATA_P = gu.read_to_string('data_adv_2021_04_p.txt')
+
+
+class TestSolutionA(unittest.TestCase):
+    """
+    unit tests for part a
+    """
+    # def test_basic(self):
+    #     """test agains the example data"""
+    #     pass
+    #
+    def test_data_p(self):
+        """test agains full data"""
+        self.assertEqual(sol.solve_a(_DATA_P), 54275)
+
+
+class TestSolutionB(unittest.TestCase):
+    """
+    unit tests for part b
+    """
+    def test_data_p(self):
+        """test agains full data"""
+        self.assertEqual(sol.solve_b(_DATA_P), 13158)
+
+
+if __name__ == '__main__':
+    unittest.main()
