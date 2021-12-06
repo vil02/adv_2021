a96400ee3d1377ff7c3524345c4496abce878dc1
piotr.idzik <vil02@o2.pl>
2021-12-06 12:46:18 +0100

    data_adv_2021_06_m.txt added

--- test_adv_2021_06.py
+++ test_adv_2021_06.py
@@ -7,6 +7,7 @@ import adv_2021_06 as sol
 import general_utils as gu
 
 _DATA_P = gu.read_to_string('data_adv_2021_06_p.txt')
+_DATA_M = gu.read_to_string('data_adv_2021_06_m.txt')
 _DATA_SMALL = '3,4,3,1,2'
 
 
@@ -32,6 +33,10 @@ class TestSolutionA(unittest.TestCase):
         """test agains full data"""
         self.assertEqual(sol.solve_a(_DATA_P), 380612)
 
+    def test_data_m(self):
+        """test agains full data"""
+        self.assertEqual(sol.solve_a(_DATA_M), 362740)
+
 
 class TestSolutionB(unittest.TestCase):
     """
@@ -44,6 +49,10 @@ class TestSolutionB(unittest.TestCase):
     def test_data_p(self):
         """test agains full data"""
         self.assertEqual(sol.solve_b(_DATA_P), 1710166656900)
+
+    def test_data_m(self):
+        """test agains full data"""
+        self.assertEqual(sol.solve_b(_DATA_M), 1644874076764)
 
 
 if __name__ == '__main__':
