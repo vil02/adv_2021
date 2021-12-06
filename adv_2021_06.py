f741f12bdc2fa98e7d5699be3c6de205977a776e
piotr.idzik <vil02@o2.pl>
2021-12-06 06:36:50 +0100

    solution of adv_2021_06 added

---
+++ adv_2021_06.py
@@ -0,0 +1,39 @@
+"""
+solution of adv_2021_06
+"""
+import functools
+
+
+def parse_input(in_str):
+    """parses the input"""
+    return [int(_) for _ in in_str.split(',')]
+
+
+@functools.lru_cache(None)
+def simulate_fish(in_value, in_days):
+    """
+    returns the number of fish produced by a fish with
+    inner counter starting at in_value after in_days
+    """
+    if in_value == 0 and in_days > 0:
+        res = simulate_fish(8, in_days-1)+simulate_fish(6, in_days-1)
+    elif in_value == 0 or in_days == 0:
+        res = 1
+    else:
+        res = simulate_fish(in_value-1, in_days-1)
+    return res
+
+
+def count_number_of_all_fish(in_value_list, in_days):
+    """returns the total number of fish after in_days"""
+    return sum(simulate_fish(_, in_days) for _ in in_value_list)
+
+
+def solve_a(in_str):
+    """solution function for part a"""
+    return count_number_of_all_fish(parse_input(in_str), 80)
+
+
+def solve_b(in_str):
+    """solution function for part b"""
+    return count_number_of_all_fish(parse_input(in_str), 256)
---
+++ test_adv_2021_06.py
@@ -0,0 +1,50 @@
+"""
+test for adv_2021_06
+"""
+
+import unittest
+import adv_2021_06 as sol
+import general_utils as gu
+
+_DATA_P = gu.read_to_string('data_adv_2021_06_p.txt')
+_DATA_SMALL = '3,4,3,1,2'
+
+
+class TestSolutionA(unittest.TestCase):
+    """
+    unit tests for part a
+    """
+    def test_parse_input(self):
+        """tests parse_input function"""
+        self.assertEqual(sol.parse_input(_DATA_SMALL), [3, 4, 3, 1, 2])
+
+    def test_count_number_of_all_fish(self):
+        """tests count_number_of_all_fish function with example data"""
+        self.assertEqual(
+            sol.count_number_of_all_fish(sol.parse_input(_DATA_SMALL), 18),
+            26)
+
+    def test_basic(self):
+        """test agains the example data"""
+        self.assertEqual(sol.solve_a(_DATA_SMALL), 5934)
+
+    def test_data_p(self):
+        """test agains full data"""
+        self.assertEqual(sol.solve_a(_DATA_P), 380612)
+
+
+class TestSolutionB(unittest.TestCase):
+    """
+    unit tests for part b
+    """
+    def test_basic(self):
+        """test agains the example data"""
+        self.assertEqual(sol.solve_b(_DATA_SMALL), 26984457539)
+
+    def test_data_p(self):
+        """test agains full data"""
+        self.assertEqual(sol.solve_b(_DATA_P), 1710166656900)
+
+
+if __name__ == '__main__':
+    unittest.main()
