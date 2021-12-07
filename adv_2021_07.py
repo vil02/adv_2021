e3d456b90958bd52115894c69fb01f78135ea87c
piotr.idzik <vil02@o2.pl>
2021-12-07 07:05:31 +0100

    code cleaning/order

--- adv_2021_07.py
+++ adv_2021_07.py
@@ -18,6 +18,13 @@ def calculate_total_fuel_a(in_position_list, in_target_pos):
     return sum(abs(_-in_target_pos) for _ in in_position_list)
 
 
+def solve_a(in_str):
+    """solution function for part a"""
+    position_list = parse_input(in_str)
+    return calculate_total_fuel_a(
+        position_list, statistics.median(position_list))
+
+
 def calculate_total_fuel_b(in_hist, in_target_pos):
     """
     returns the amout of fuel needed to align
@@ -39,13 +46,6 @@ def find_minimal_fuel_usage_hist(in_position_hist, in_cost_fun):
     max_val = max(in_position_hist)
     return min(
         in_cost_fun(in_position_hist, _) for _ in range(min_val, max_val+1))
-
-
-def solve_a(in_str):
-    """solution function for part a"""
-    position_list = parse_input(in_str)
-    return calculate_total_fuel_a(
-        position_list, statistics.median(position_list))
 
 
 def _calculate_histogram(in_num_list):
