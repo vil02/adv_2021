3148b9ae453fb52c512bbed8edf1e5cc8095fa93
piotr.idzik <vil02@o2.pl>
2021-12-07 17:59:54 +0100

    data_adv_2021_03_small added

--- tests/test_adv_2021_03.py
+++ tests/test_adv_2021_03.py
@@ -6,19 +6,9 @@ import unittest
 import general_utils as gu
 import solutions.adv_2021_03 as sol
 
-_TEST_DATA = [
-    "00100",
-    "11110",
-    "10110",
-    "10101",
-    "01111",
-    "10111",
-    "00111",
-    "11100",
-    "10000",
-    "11001",
-    "00010",
-    "01010"]
+
+def _data_small():
+    return gu.read_input(3, 'small')
 
 
 def _data_p():
@@ -35,15 +25,15 @@ class TestSolutionA(unittest.TestCase):
     """
     def test_calculate_gamma(self):
         """tests calculate_gamma agains the example data"""
-        self.assertEqual(sol.calculate_gamma(_TEST_DATA), 22)
+        self.assertEqual(sol.calculate_gamma(_data_small().split()), 22)
 
     def test_calculate_epsilon(self):
         """tests calculate_epsilon agains the example data"""
-        self.assertEqual(sol.calculate_epsilon(_TEST_DATA), 9)
+        self.assertEqual(sol.calculate_epsilon(_data_small().split()), 9)
 
     def test_example_data(self):
         """tests solution function agains the example data"""
-        self.assertEqual(sol.solve_a('\n'.join(_TEST_DATA)), 198)
+        self.assertEqual(sol.solve_a(_data_small()), 198)
 
     def test_data_p(self):
         """test agains full data"""
@@ -60,15 +50,15 @@ class TestSolutionB(unittest.TestCase):
     """
     def test_calculate_oxygen(self):
         """tests calculate_gamma agains the example data"""
-        self.assertEqual(sol.calculate_oxygen(_TEST_DATA), 23)
+        self.assertEqual(sol.calculate_oxygen(_data_small().split()), 23)
 
     def test_calculate_co2(self):
         """tests calculate_epsilon agains the example data"""
-        self.assertEqual(sol.calculate_co2(_TEST_DATA), 10)
+        self.assertEqual(sol.calculate_co2(_data_small().split()), 10)
 
     def test_example_data(self):
         """tests solution function agains the example data"""
-        self.assertEqual(sol.solve_b('\n'.join(_TEST_DATA)), 230)
+        self.assertEqual(sol.solve_b(_data_small()), 230)
 
     def test_data_p(self):
         """test agains full data"""
