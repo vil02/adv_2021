2e7823e87c2f3f8680ff149a63384d7bf36d7aba
piotr.idzik <vil02@o2.pl>
2021-12-07 07:03:23 +0100

    histogram used in part b

--- adv_2021_07.py
+++ adv_2021_07.py
@@ -11,33 +11,34 @@ def parse_input(in_str):
 
 def calculate_total_fuel_a(in_position_list, in_target_pos):
     """
-    returns the amout of fules needed to align
+    returns the amout of fuel needed to align
     in_position_list to in_target_pos
     as described in part_a
     """
     return sum(abs(_-in_target_pos) for _ in in_position_list)
 
 
-def calculate_total_fuel_b(in_numbers, in_target_pos):
+def calculate_total_fuel_b(in_hist, in_target_pos):
     """
-    returns the amout of fules needed to align
-    in_position_list to in_target_pos
+    returns the amout of fuel needed to align
+    all positions from in_hist to in_target_pos
     as described in part_b
     """
     def cost(in_val):
         return ((in_val+1)*in_val)//2
-    return sum(cost(abs(_-in_target_pos)) for _ in in_numbers)
+    return sum(
+        count*cost(abs(pos-in_target_pos)) for [pos, count] in in_hist.items())
 
 
-def find_minimal_fuel_usage(in_position_list, in_cost_fun):
+def find_minimal_fuel_usage_hist(in_position_hist, in_cost_fun):
     """
     finds the minimal fuel usage in order to aligh all submarines with starting
     positions as in_position_list
     """
-    min_val = min(in_position_list)
-    max_val = max(in_position_list)
+    min_val = min(in_position_hist)
+    max_val = max(in_position_hist)
     return min(
-        in_cost_fun(in_position_list, _) for _ in range(min_val, max_val+1))
+        in_cost_fun(in_position_hist, _) for _ in range(min_val, max_val+1))
 
 
 def solve_a(in_str):
@@ -47,6 +48,17 @@ def solve_a(in_str):
         position_list, statistics.median(position_list))
 
 
+def _calculate_histogram(in_num_list):
+    res = {}
+    for _ in in_num_list:
+        if _ in res:
+            res[_] += 1
+        else:
+            res[_] = 1
+    return res
+
+
 def solve_b(in_str):
     """solution function for part b"""
-    return find_minimal_fuel_usage(parse_input(in_str), calculate_total_fuel_b)
+    return find_minimal_fuel_usage_hist(
+        _calculate_histogram(parse_input(in_str)), calculate_total_fuel_b)
