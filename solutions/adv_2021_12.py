332d826ed959a84e634fb330678cf2a0fb9f7504
piotr.idzik <vil02@o2.pl>
2021-12-12 06:54:37 +0100

    cleaning/linting

--- solutions/adv_2021_12.py
+++ solutions/adv_2021_12.py
@@ -5,14 +5,17 @@ import copy
 
 
 def parse_input(in_str):
+    """
+    parses the input string in_str into a dictionary representing a graph
+    res['node_name'] is a list of all nodes,
+    which can be direcly accessed from the given one
+    """
     result = {}
 
-    def update_res(cave_a, cave_b):
-        if cave_a in result:
-            result[cave_a].append(cave_b)
-        else:
-            result[cave_a] = [cave_b]
-
+    def update_res(in_first_cave, in_second_cave):
+        if in_first_cave not in result:
+            result[in_first_cave] = []
+        result[in_first_cave].append(in_second_cave)
     for _ in in_str.splitlines():
         cave_a, cave_b = _.split('-')
         update_res(cave_a, cave_b)
@@ -21,9 +24,14 @@ def parse_input(in_str):
 
 
 def is_cave_small(in_cave):
+    """checks if the in_case is small"""
     return in_cave.islower()
 
-def find_number_of_paths(in_data):
+
+def find_number_of_paths_a(in_data):
+    """
+    returns the number of paths as described in part a
+    """
     path_list = []
 
     def inner(cur_cave, cur_path, visited_small_caves):
@@ -32,9 +40,9 @@ def find_number_of_paths(in_data):
         else:
             for new_cave in in_data[cur_cave]:
                 if (is_cave_small(new_cave) and new_cave not in cur_path):
-                    tmp_visited_small_caves = copy.deepcopy(visited_small_caves)
-                    tmp_visited_small_caves.add(new_cave)
-                    inner(new_cave, cur_path+[new_cave], tmp_visited_small_caves)
+                    inner(
+                        new_cave, cur_path+[new_cave],
+                        visited_small_caves | set(new_cave))
                 elif not is_cave_small(new_cave):
                     inner(new_cave, cur_path+[new_cave], visited_small_caves)
     inner('start', ['start'], set())
@@ -42,36 +50,42 @@ def find_number_of_paths(in_data):
 
 
 def find_number_of_paths_b(in_data):
+    """
+    returns the number of paths as described in part b
+    """
     path_list = []
 
     def is_worth_visit(in_cave, in_visited_dict):
         assert is_cave_small(in_cave)
-        return in_cave not in in_visited_dict or all(_ < 2 for _ in in_visited_dict.values())
+        return in_cave not in in_visited_dict \
+            or all(_ < 2 for _ in in_visited_dict.values())
 
     def inner(cur_cave, cur_path, visited_small_caves):
         if cur_cave == 'end':
             path_list.append(cur_path)
         else:
             for new_cave in in_data[cur_cave]:
-                if is_cave_small(new_cave) and new_cave != 'start' and is_worth_visit(new_cave, visited_small_caves):
-                    tmp_visited_small_caves = copy.deepcopy(visited_small_caves)
+                if is_cave_small(new_cave) and new_cave != 'start' \
+                        and is_worth_visit(new_cave, visited_small_caves):
+                    tmp_visited_small_caves = \
+                        copy.deepcopy(visited_small_caves)
                     if new_cave in tmp_visited_small_caves:
                         tmp_visited_small_caves[new_cave] += 1
                     else:
                         tmp_visited_small_caves[new_cave] = 1
-                    inner(new_cave, cur_path+[new_cave], tmp_visited_small_caves)
+                    inner(
+                        new_cave, cur_path+[new_cave], tmp_visited_small_caves)
                 elif not is_cave_small(new_cave):
                     inner(new_cave, cur_path+[new_cave], visited_small_caves)
     inner('start', ['start'], {})
     return len(path_list)
 
+
 def solve_a(in_str):
     """solution function for part a"""
-    data = parse_input(in_str)
-    return find_number_of_paths(data)
+    return find_number_of_paths_a(parse_input(in_str))
 
 
 def solve_b(in_str):
     """solution function for part b"""
-    data = parse_input(in_str)
-    return find_number_of_paths_b(data)
+    return find_number_of_paths_b(parse_input(in_str))
