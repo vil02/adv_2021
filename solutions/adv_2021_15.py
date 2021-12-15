69944c7406aedcc88916b2398b66893cc9055a0e
piotr.idzik <vil02@o2.pl>
2021-12-15 08:56:27 +0100

    more  tests, linting

--- solutions/adv_2021_15.py
+++ solutions/adv_2021_15.py
@@ -1,28 +1,27 @@
 """
 solution of adv_2021_15
 """
-import itertools
-import functools
 import copy
 import heapq
 
 
 def parse_input(in_str):
+    """
+    Parses the input.
+    Returns the "list of rows". Each row is a list of numbers
+    """
     def proc_row(in_row):
         return [int(_) for _ in in_row]
-    return [proc_row(_) for _ in in_str.splitlines()]
+    res = [proc_row(_) for _ in in_str.splitlines()]
+    assert len({len(_) for _ in res}) == 1
+    return res
 
 
 def get_value(in_data, in_pos):
+    """
+    returns the value storen in in_data in in_pos[0] row and in_pos[1] column
+    """
     return in_data[in_pos[0]][in_pos[1]]
-
-
-# def get_new_fields(in_data, in_pos):
-#     # res = []
-#     # if in_pos[0] < len(in_data)-1:
-#     #     res.append((in_pos[0]+1, in_pos[1]))
-#     # if in_pos[1] < len(in_data[0])-1:
-#     #     res.append((in_pos[0], in_pos[1]+1))
 
 
 def get_all_adjacent_positions(in_data, in_center_pos):
@@ -41,54 +40,18 @@ def get_all_adjacent_positions(in_data, in_center_pos):
     return [_ for _ in search_range if is_correct(_)]
 
 
-# def find_minimum_risk(in_data):
-#     visited = {}
-#
-#     def inner(cur_pos, in_path):
-#         if (cur_pos, in_path) in visited:
-#             res = visited[(cur_pos, in_path)]
-#         elif cur_pos[0] == len(in_data)-1 and cur_pos[1] == len(in_data[-1])-1:
-#             res = get_value(in_data, cur_pos)
-#         else:
-#             min_val = math.inf
-#             for _ in get_all_adjacent_positions(in_data, cur_pos):
-#                 if _ not in in_path:
-#                     min_val = min(min_val, inner(_, tuple(list(in_path)+[_])))
-#             res = get_value(in_data, cur_pos) + min_val
-#             visited[(cur_pos, in_path)] = res
-#         return res
-#
-#     res =inner((0, 0), ((0, 0),))-get_value(in_data, (0, 0))
-#     print(visited)
-#     return res
-#
-# def find_minimum_risk(in_data):
-#     visited = {}
-#
-#     def inner(cur_pos):
-#         if cur_pos in visited:
-#             res = visited[cur_pos]
-#         elif cur_pos[0] == len(in_data)-1 and cur_pos[1] == len(in_data[-1])-1:
-#             res = get_value(in_data, cur_pos)
-#         else:
-#             min_val = math.inf
-#             for _ in get_all_adjacent_positions(in_data, cur_pos):
-#                 min_val = min(min_val, inner(_))
-#             res = get_value(in_data, cur_pos) + min_val
-#             visited[cur_pos] = res
-#         return res
-#
-#     return inner((0, 0))-get_value(in_data, (0, 0))
-
 def find_minimum_risk(in_data):
+    """
+    returns the minimum risk of going from the
+    upper-left to the lower-right corner of the in_data
+    """
     known_states = [(0, (0, 0))]
-
     visited = set()
     while True:
         cur_risk, cur_pos = heapq.heappop(known_states)
         if cur_pos in visited:
             continue
-        if cur_pos == (len(in_data)-1, len(in_data[0])-1):
+        if cur_pos == (len(in_data)-1, len(in_data[cur_pos[0]])-1):
             return cur_risk
         visited.add(cur_pos)
         for new_pos in get_all_adjacent_positions(in_data, cur_pos):
@@ -98,50 +61,44 @@ def find_minimum_risk(in_data):
                     (cur_risk + get_value(in_data, new_pos), new_pos))
 
 
-
 def solve_a(in_str):
     """solution function for part a"""
-    data = parse_input(in_str)
-    return find_minimum_risk(data)
+    return find_minimum_risk(parse_input(in_str))
 
 
 def make_plus(in_data):
+    """
+    Increases every value in in_data by 1.
+    If the new value is >= 9 it sets it to 1
+    """
     def proc_single(in_row):
-        def proc_val(in_val):
-            res = in_val+1
-            if res >= 10:
-                res = 1
-            return res
-        return [proc_val(_) for _ in in_row]
+        return [_ % 9+1 for _ in in_row]
     return [proc_single(_) for _ in in_data]
 
 
-def merge_hor(in_data_a, in_data_b):
-    res = [a+b for (a, b) in zip(in_data_a, in_data_b)]
+def extend_data(in_data):
+    """
+    extends the in_data for the part_b
+    """
+    def merge_hor(in_data_a, in_data_b):
+        return [a+b for (a, b) in zip(in_data_a, in_data_b)]
 
-    return res
-
-
-def merge_vert(in_data_a, in_data_b):
-    res = in_data_a+in_data_b
-
-    return res
-
-def generate_big_data(in_data):
+    def merge_vert(in_data_a, in_data_b):
+        return in_data_a+in_data_b
     tmp_data = copy.deepcopy(in_data)
     res_data = copy.deepcopy(in_data)
-    for _ in range(4):
+    extension_size = 4
+    for _ in range(extension_size):
         tmp_data = make_plus(tmp_data)
         res_data = merge_hor(res_data, tmp_data)
 
     tmp_data = copy.deepcopy(res_data)
-    for _ in range(4):
+    for _ in range(extension_size):
         tmp_data = make_plus(tmp_data)
         res_data = merge_vert(res_data, tmp_data)
     return res_data
 
+
 def solve_b(in_str):
     """solution function for part b"""
-    data = parse_input(in_str)
-    data = generate_big_data(data)
-    return find_minimum_risk(data)
+    return find_minimum_risk(extend_data(parse_input(in_str)))
--- tests/test_adv_2021_15.py
+++ tests/test_adv_2021_15.py
@@ -11,6 +11,10 @@ def _data_small():
     return gu.read_input(15, 'small')
 
 
+def _data_small_b():
+    return gu.read_input(15, 'small_b')
+
+
 def _data_p():
     return gu.read_input(15, 'p')
 
@@ -23,6 +27,10 @@ class TestSolutionA(unittest.TestCase):
         """test agains example data"""
         self.assertEqual(sol.solve_a(_data_small()), 40)
 
+    def test_data_small_b(self):
+        """test agains extented example data"""
+        self.assertEqual(sol.solve_a(_data_small_b()), 315)
+
     def test_data_p(self):
         """test agains full data"""
         self.assertEqual(sol.solve_a(_data_p()), 714)
@@ -32,6 +40,12 @@ class TestSolutionB(unittest.TestCase):
     """
     unit tests for part b
     """
+    def test_extend_data(self):
+        """test of extend_data"""
+        self.assertEqual(
+            sol.extend_data(sol.parse_input(_data_small())),
+            sol.parse_input(_data_small_b()))
+
     def test_data_small(self):
         """test agains example data"""
         self.assertEqual(sol.solve_b(_data_small()), 315)
