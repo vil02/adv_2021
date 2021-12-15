872ac94d52dc9ac54723580d5ea281b1ce47d024
piotr.idzik <vil02@o2.pl>
2021-12-15 07:44:35 +0100

    messy solution of adv_2021_15 added

---
+++ tests/test_adv_2021_15.py
@@ -0,0 +1,44 @@
+"""
+test for adv_2021_15
+"""
+
+import unittest
+import general_utils as gu
+import solutions.adv_2021_15 as sol
+
+
+def _data_small():
+    return gu.read_input(15, 'small')
+
+
+def _data_p():
+    return gu.read_input(15, 'p')
+
+
+class TestSolutionA(unittest.TestCase):
+    """
+    unit tests for part a
+    """
+    def test_data_small(self):
+        """test agains example data"""
+        self.assertEqual(sol.solve_a(_data_small()), 40)
+
+    def test_data_p(self):
+        """test agains full data"""
+        self.assertEqual(sol.solve_a(_data_p()), 714)
+
+
+class TestSolutionB(unittest.TestCase):
+    """
+    unit tests for part b
+    """
+    def test_data_small(self):
+        """test agains example data"""
+        self.assertEqual(sol.solve_b(_data_small()), 315)
+    def test_data_p(self):
+        """test agains full data"""
+        self.assertEqual(sol.solve_b(_data_p()), -1)
+
+
+if __name__ == '__main__':
+    unittest.main()
