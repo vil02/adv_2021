977fdbc23884cddd0d56a33569d29a6d251a8a48
Piotr Idzik <vil02@o2.pl>
2021-12-20 17:31:37 +0100

    get_x_vel_list added

--- solutions/adv_2021_17.py
+++ solutions/adv_2021_17.py
@@ -5,18 +5,48 @@ import functools
 import itertools
 
 
-def get_next_state(in_pos, in_vel):
-    """returns the next position and nect velocity"""
-    res_pos = tuple(p+v for (p, v) in zip(in_pos, in_vel))
-    if in_vel[0] != 0:
-        if in_vel[0] > 0:
-            res_vel_x = in_vel[0]-1
+def get_next_state_x(in_x, in_vel_x):
+    """returns the next position and velocity in x-dir"""
+    res_x = in_x+in_vel_x
+    if in_vel_x != 0:
+        if in_vel_x > 0:
+            res_vel_x = in_vel_x-1
         else:
-            res_vel_x = in_vel[0]+1
+            res_vel_x = in_vel_x+1
     else:
-        res_vel_x = in_vel[0]
-    res_vel = (res_vel_x, in_vel[1]-1)
-    return (res_pos, res_vel)
+        res_vel_x = 0
+    return res_x, res_vel_x
+
+
+def get_next_state_y(in_y, in_vel_y):
+    """returns the next position and velocity in y-dir"""
+    return in_y+in_vel_y, in_vel_y-1
+
+
+def is_between(in_val, in_range):
+    """checks if in_val is in_range/interval"""
+    return in_range[0] <= in_val <= in_range[1]
+
+
+def is_hit_x(in_x, in_vel_x, in_target_data):
+    """checks is in_x/in_vel_x will hit in_target_data"""
+    assert in_vel_x >= 0
+    if is_between(in_x, in_target_data):
+        res = True
+    elif in_x > in_target_data[1]:
+        res = False
+    elif in_x < in_target_data[0] and in_vel_x == 0:
+        res = False
+    else:
+        res = is_hit_x(*get_next_state_x(in_x, in_vel_x), in_target_data)
+    return res
+
+
+def get_next_state(in_pos, in_vel):
+    """returns the next position and next velocity"""
+    res_pos_x, res_vel_x = get_next_state_x(in_pos[0], in_vel[0])
+    res_pos_y, res_vel_y = get_next_state_y(in_pos[1], in_vel[1])
+    return ((res_pos_x, res_pos_y), (res_vel_x, res_vel_y))
 
 
 def simulate(in_pos, in_vel, in_target_data):
@@ -25,11 +55,8 @@ def simulate(in_pos, in_vel, in_target_data):
     returns the information if it will hit the target and the flight path
     """
     def is_in_target(in_pos, in_target_data):
-        def is_between(in_val, in_range):
-            return in_range[0] <= in_val <= in_range[1]
-        return all(
-            is_between(in_pos[_], in_target_data[_])
-            for _ in range(len(in_pos)))
+        return is_between(in_pos[0], in_target_data[0]) \
+            and is_between(in_pos[1], in_target_data[1])
     assert 0 < in_target_data[0][0] < in_target_data[0][1]
     assert in_vel[0] >= 0
     path = tuple()
@@ -57,11 +84,15 @@ def get_all_hitting_paths(in_target_data):
         if (res*(res+1))//2 <= in_target_min_x:
             res += 1
         return res-1
-    min_x_vel = find_min_x_vel(in_target_data[0][0])
-    max_x_vel = in_target_data[0][1]+1
+
+    def get_x_vel_list(in_target_data_x):
+        min_x_vel = find_min_x_vel(in_target_data_x[0])
+        max_x_vel = in_target_data_x[1]+1
+        return [_ for _ in range(min_x_vel, max_x_vel)
+                if is_hit_x(0, _, in_target_data_x)]
     max_y_vel = 2*abs(in_target_data[1][1])+1
     vel_range = itertools.product(
-        range(min_x_vel, max_x_vel), range(-max_y_vel, max_y_vel))
+        get_x_vel_list(in_target_data[0]), range(-max_y_vel, max_y_vel))
     res_list = []
     for _ in vel_range:
         is_hit, cur_path = simulate((0, 0), _, in_target_data)
