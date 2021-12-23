e8cd76776757c571e51921972cd4ebd6693c1577
Piotr Idzik <vil02@o2.pl>
2021-12-23 17:32:47 +0100

    parse_input added

--- solutions/adv_2021_23.py
+++ solutions/adv_2021_23.py
@@ -3,6 +3,15 @@ solution of adv_2021_23
 """
 
 
+def parse_input(in_str):
+    char_list = [_ for _ in in_str if _ in {'A', 'B', 'C', 'D'}]
+    assert len(char_list) == 8
+    room_a = [char_list[0], char_list[4]]
+    room_b = [char_list[1], char_list[5]]
+    room_c = [char_list[2], char_list[6]]
+    room_d = [char_list[3], char_list[7]]
+    return room_a, room_b, room_c, room_d
+
 # def solve_a(in_str):
 #     """solution function for part a"""
 #     pass
--- tests/test_adv_2021_23.py
+++ tests/test_adv_2021_23.py
@@ -4,7 +4,7 @@ test for adv_2021_23
 
 import unittest
 import general_utils as gu
-# import solutions.adv_2021_23 as sol
+import solutions.adv_2021_23 as sol
 
 
 def _data_small():
@@ -19,6 +19,13 @@ class TestSolutionA(unittest.TestCase):
     """
     unit tests for part a
     """
+    def test_parse_input(self):
+        """test function parse_input against example data"""
+        room_a, room_b, room_c, room_d = sol.parse_input(_data_small())
+        self.assertEqual(room_a, ['B', 'A'])
+        self.assertEqual(room_b, ['C', 'D'])
+        self.assertEqual(room_c, ['B', 'C'])
+        self.assertEqual(room_d, ['D', 'A'])
 #    def test_data_small(self):
 #        """test against example data"""
 #        self.assertEqual(sol.solve_a(_data_small()), 12521)
