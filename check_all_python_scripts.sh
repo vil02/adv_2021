cd68fc2e1965c894bcccbcf409f1cc72db6d3ca9
piotr.idzik <vil02@o2.pl>
2022-09-13 21:56:00 +0200

    fix: suppress E203 for flake8

--- check_all_python_scripts.sh
+++ check_all_python_scripts.sh
@@ -13,7 +13,10 @@ function check_single()
     fi
 
     printf "Checking with flake8:\n"
-    if ! poetry run flake8 "$in_path" --count --max-line-length=80 --show-source ; then
+    declare -r flake8_opts="--count --max-line-length=80 --show-source --ignore=E203"
+    declare -r flake8_cmd="poetry run flake8 $in_path $flake8_opts"
+    declare -r flake8_res=$($flake8_cmd)
+    if [ "$flake8_res" -ne 0 ]; then
         result_val=1
     fi
     printf "...done\n\n\n"
