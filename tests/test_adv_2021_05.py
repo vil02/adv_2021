8d93b68dea24d45010fbba742ef1e81e1642df92
piotr.idzik <vil02@o2.pl>
2023-07-23 12:00:22 +0200

    tests: add test data for `adv_2021_05`

--- tests/test_adv_2021_05.py
+++ tests/test_adv_2021_05.py
@@ -7,16 +7,24 @@ import general_utils as gu
 import solutions.adv_2021_05 as sol
 
 
+def _read_input(input_id):
+    return gu.read_input(5, input_id)
+
+
 def _data_small():
-    return gu.read_input(5, "small")
+    return _read_input("small")
 
 
 def _data_p():
-    return gu.read_input(5, "p")
+    return _read_input("p")
 
 
 def _data_m():
-    return gu.read_input(5, "m")
+    return _read_input("m")
+
+
+def _data_s():
+    return _read_input("s")
 
 
 class TestSolutionA(unittest.TestCase):
@@ -36,6 +44,10 @@ class TestSolutionA(unittest.TestCase):
         """test against full data"""
         self.assertEqual(sol.solve_a(_data_m()), 3990)
 
+    def test_data_s(self):
+        """test against full data"""
+        self.assertEqual(sol.solve_a(_data_s()), 7473)
+
 
 class TestSolutionB(unittest.TestCase):
     """
@@ -53,6 +65,10 @@ class TestSolutionB(unittest.TestCase):
     def test_data_m(self):
         """test against full data"""
         self.assertEqual(sol.solve_b(_data_m()), 21305)
+
+    def test_data_s(self):
+        """test against full data"""
+        self.assertEqual(sol.solve_b(_data_s()), 24164)
 
 
 if __name__ == "__main__":
